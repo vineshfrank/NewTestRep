0c91c472d71eb28bbefa0088535f86dbc3d4d4c0
Arun <Arun@Arun-PC>
2015-08-26 21:27:50 +0530

    my project

---
+++ mylearning/src/main/java/devopes/mylearning/test/App.java
@@ -0,0 +1,13 @@
+package devopes.mylearning.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
